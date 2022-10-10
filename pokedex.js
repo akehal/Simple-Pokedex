23629b8dbfdab4e1deb05dbae37a547d19f30124
akehal <akehal@uwo.ca>
2022-10-09 20:32:17 -0400

    change styles, add ul and div element
    add ul and div element, change css styles

--- pokedex.js
+++ pokedex.js
@@ -1,3 +1,39 @@
+const searchNum = document.getElementById('pokeNum');
+const searchName = document.getElementById('pokeName');
+var a, i, txtvalue;
+searchName.value.toLowerCase();
+searchNum.value.toLowerCase();
+ul = document.getElementById("pokeUL");
+li = document.getElementsByTagName('li');
+
+for(i = 0l i < li.length; i++){
+    a = li[i].getElementsByTagName("a")[0];
+    txtvalue
+}
+
+search.addEventListener("input", (e) =>{
+    let value = e.target.value;
+    if(value && value.trim().length >0){
+        value = value.trim().toLowerCase();
+        setList(pokemonArray.filter(pokemon=>{
+            return pokemon.name.includes(value)
+        }))
+    }else{
+
+    }
+})
+
+function setList(results){
+    for(const pokemon of results){
+        const listItem = document.createElement('li')
+        listItem.classList.add('list-item');
+        const text = document.createTextNode(pokemon.name);
+        listItem.appendChild(text);
+        list.appendChild(listItem);
+    }
+}
+
+
 var pokemonArray = [
     {name: "Bulbasaur", number: "001", rarity: "4", typeCombo: "Grass / Poison", fastMoves: "Tackle / Vine Whip"},
     {name: "Ivysaur", number: "002", rarity: "5", typeCombo: "Grass / Poison", fastMoves: "Razor Leaf / Vine Whip"},
@@ -25,6 +61,8 @@ var pokemonArrayDisplay = [];
 var counter = 0;
 var finalOuput = "" 
 
+
+
 function showAlertNum(){ //gets called when user types in a pokemon number
     if(pokeNum.value>0 && pokeNum.value <= 20){
         for(let i in pokemonArray){ //iterates through array and pushes object if object includes the user's input
@@ -43,7 +81,7 @@ function showAlertNum(){ //gets called when user types in a pokemon number
         alert(finalOuput); //outputs an alert box with the first 5 objects that matched
     }
     else{
-        alert("The number you have input is not between 0 and 20, please try again!")
+        alert("The number you have input is not between 1 and 20, please try again!")
     }
     }
     
@@ -71,7 +109,14 @@ function showAlertName(){ //gets called when user types in a pokemon name
     }
 }
 
+
 let divElement = document.createElement('div');
+//console.dir(divElement);
+let textNode = document.createTextNode("HELLO THIS IS CONTENT");
+divElement.appendChild(textNode);
+let divContainer = document.querySelector('.container');
+divContainer.appendChild(divElement);
+
 
 //pokemonArray[i].name.toLowerCase() == pokeName.value.toLowerCase() || 
 
